f807e4ba627274d2a9c2f1076fe585cded944080
Fkmutai <mutaimossi@gmail.com>
2026-02-21 09:45:52 +0300

    Add files via upload

---
+++ EvenNumbers.java
@@ -0,0 +1,11 @@
+    public class EvenNumbers {
+    public static void main(String[] args) {
+        System.out.println("Even numbers between 20 and 100:");
+
+        // Start at 20, continue while i is <= 100, increment by 2
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
